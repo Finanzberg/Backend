eb2ace91c630ef63f13f37491dce8875cfc15aa5
moritz <moritzmeissgeier@gmail.com>
2024-02-19 12:57:44 +0100

    Added BankStatements
    Descriptions for payments of all kind

--- src/main/java/de/finanzberg/backend/db/BankStatementCategory.java
+++ src/main/java/de/finanzberg/backend/db/BankStatementCategory.java
@@ -1,5 +1,24 @@
 package de.finanzberg.backend.db;
 
 public enum BankStatementCategory {
+
+    // EXPENDITURE
     BASIC_NEEDS,
+    MOBILITY,
+    SHOPPING, 
+    HEALTH,
+    FREETIME, 
+    RENT,
+    INSURANCE,
+    PERSONAL_PAYMENT,
+    SUBSCRIPTIONS,
+    OTHER,
+
+    // INCOME
+    SALARY,
+    PERSONAL_PAYMENT,
+    UNEMPLOYMENT_BENEFIT,
+    CHILD_BENEFIT,
+    OTHER,
+    
 }
